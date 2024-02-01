1e2cafdabe3287cadd452f849e286850f2e0d261
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2024-02-01 19:43:02 +0530

    next

--- src/app/page/view-all-books/view-all-books.component.ts
+++ src/app/page/view-all-books/view-all-books.component.ts
@@ -1,27 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-all-books',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,FormsModule,CommonModule],
   templateUrl: './view-all-books.component.html',
   styleUrl: './view-all-books.component.css'
 })
-export class ViewAllBooksComponent implements OnInit{
-    private http;
-    public bookList:any ={};
+export class ViewAllBooksComponent implements OnInit {
+  private http;
+  public bookList: any = {};
 
-    constructor(private httpCliant:HttpClient){
-      this.http=httpCliant;
-    }
-    ngOnInit(): void {
-        this.loadBooks();
-    }
-    loadBooks() {
-      this.http.get('http://localhost:8080/book/get').subscribe((data) => {
-        this.bookList = data; // Assuming that the response is assigned to bookList
-        console.log(this.bookList);
-      });
-    }
+  constructor(private httpClient: HttpClient) {
+    this.http = httpClient;
+  }
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    this.http.get('http://localhost:8080/book/get').subscribe((data) => {
+      console.log(data);
+      this.bookList = data;
+    });
+  }
 }
+
