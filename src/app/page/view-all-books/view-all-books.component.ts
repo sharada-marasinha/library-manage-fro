747da0a012d2af4b5f93a36196a2cdce1be37e60
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2024-02-05 22:56:18 +0530

    DELETE API done

--- src/app/page/view-all-books/view-all-books.component.ts
+++ src/app/page/view-all-books/view-all-books.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-view-all-books',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,FormsModule,CommonModule],
   templateUrl: './view-all-books.component.html',
   styleUrl: './view-all-books.component.css'
 })
 export class ViewAllBooksComponent implements OnInit{
     private http;
     public bookList:any ={};
+    public selectedBook : any;
 
     constructor(private httpCliant:HttpClient){
       this.http=httpCliant;
@@ -20,8 +25,21 @@ export class ViewAllBooksComponent implements OnInit{
     }
     loadBooks() {
       this.http.get('http://localhost:8080/book/get').subscribe((data) => {
-        this.bookList = data; // Assuming that the response is assigned to bookList
+        this.bookList = data;
         console.log(this.bookList);
       });
+    }
+    deleteBook(){
+      let api = "http://localhost:8080/book/"+this.selectedBook.id;
+      this.http.delete(api,{responseType:'text'}).subscribe((responce:string) => {
+        console.log(responce);
+        this.loadBooks();
+        this.selectedBook=null;
+      });
+    }
+
+    setSelectedBook(book:any){
+      this.selectedBook=book;
+      console.log("setSelectedBook"+book.id);
     }
 }
