1363b65e48bc25ba13ee739de0adbe67494d5e4e
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2024-02-08 12:39:20 +0530

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './page/login/login.component';
+import { ViewAllBooksComponent } from './page/view-all-books/view-all-books.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"login",
+        component:LoginComponent
+    },
+    {
+        path:"view-all-books",
+        component:ViewAllBooksComponent
+    }
+];
